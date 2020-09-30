b2ca5a5f5b87d0c3d0e03b88839aede44d730667
jester <j+gitkraken.com@jesterweb.de>
2020-09-30 22:48:43 +0200

    Use HTML5 field types for inputs

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, \
-    FloatField, DateField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.fields.html5 import DecimalField, DateField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, \
     Optional
 from app.models import User
@@ -36,11 +36,11 @@ class RegistrationForm(FlaskForm):
 class MeasurementForm(FlaskForm):
     date = DateField('Messdatum', validators=[DataRequired()],
                      default=date.today)
-    weight = FloatField('Gewicht [kg]')
-    bmi = FloatField('BMI', validators=[Optional()])
-    body_Fat = FloatField('Koerperfett [%]', validators=[Optional()])
-    muscle = FloatField('Muskelmasse [%]', validators=[Optional()])
-    rm_kcal = FloatField('Grundumsatz [kcal]', validators=[Optional()])
-    visceral_fat = FloatField('Viszeralfett', validators=[Optional()])
-    circumference = FloatField('Bauchumfang [cm]', validators=[Optional()])
+    weight = DecimalField('Gewicht [kg]')
+    bmi = DecimalField('BMI', validators=[Optional()])
+    body_Fat = DecimalField('Koerperfett [%]', validators=[Optional()])
+    muscle = DecimalField('Muskelmasse [%]', validators=[Optional()])
+    rm_kcal = DecimalField('Grundumsatz [kcal]', validators=[Optional()])
+    visceral_fat = DecimalField('Viszeralfett', validators=[Optional()])
+    circumference = DecimalField('Bauchumfang [cm]', validators=[Optional()])
     submit = SubmitField('Speichern', validators=[Optional()])
